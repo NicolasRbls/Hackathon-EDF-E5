4d96c35b99ea190e1a600995535da04db33474a8
Manon <mmars2ajaccio@gmail.com>
2025-12-11 11:43:35 +0100

    variables CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="min-h-screen bg-white dark:bg-zinc-900 dark:text-gray-200">
-      <div class="h-full w-full max-w-4xl m-auto">
+      <div class="h-full w-full max-w-4xl m-auto dark:bg-zinc-900">
         <router-view />
       </div>
   </div>
